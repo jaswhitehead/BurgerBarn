a1803872af6b2ed165dd571d3be6f6443ae61058
Jason Whitehead <jaswhitehead@gmail.com>
2020-07-19 20:09:05 -0500

    Ran NPM init and installed all necessary dependencies. Also set up server.js and the schema/seeds files.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", false), ("Southwest Burger", false), ("Bacon Cheeseburger", false);
